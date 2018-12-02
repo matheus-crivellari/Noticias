d2b680e0d39af9c54ea22bb1f144e51965ce07b5
Matheus Crivellari <matheus.acrivellari@gmail.com>
2018-12-02 19:52:11 -0200

    Home do site com placeholders de imagem.

---
+++ php/footer.php
@@ -0,0 +1,9 @@
+<footer id="footer">
+	<div id="copyright">
+		© Copyright 2018 - Nome do Site - Todos os direitos reservados.
+	</div>
+	<div id="creditos">
+		Desenvolvido por: Nome do Site
+	</div>
+	<div class="clearfix"></div>
+</footer>
---
+++ php/header.php
@@ -0,0 +1,28 @@
+<nav id="social">
+	<ul>
+		<li><a href="#"><i class="fab fa-facebook"></i></a></li>
+		<li><a href="#"><i class="fab fa-instagram"></i></a></li>
+		<li><a href="#"><i class="fab fa-twitter"></i></a></li>
+		<li><a href="#"><i class="fab fa-youtube"></i></a></li>
+		<li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+	</ul>
+	<div class="clearfix"></div>
+</nav>
+<header id="header">
+	<div id="logo"></div>
+	<div id="busca">
+		<form action="buscar.php" action="post">
+			<input type="search" id="buscar" name="buscar" placeholder="Buscar">
+			<input type="submit" id="ir" value="Ir">
+		</form>
+	</div>
+	<div class="clearfix"></div>
+</header>
+<nav id="menu">
+	<ul>
+		<li><a href="#">Home</a></li>
+		<li><a href="#">Notícias</a></li>
+		<li><a href="#">Administração</a></li>
+	</ul>
+	<div class="clearfix"></div>
+</nav>
